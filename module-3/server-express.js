8090a82fca6ebe19100147fa3cf493f39797753b
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-09-21 04:28:09 +0600

    done How to render html file & handle JSON data. but not to connect mongodb couse of it is just express text

--- module-3/server-express.js
+++ module-3/server-express.js
@@ -2,16 +2,23 @@ const express = require('express')
  // creating an express instance
 const app = express()
 
+app.use(express.json());
 
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.json(
-        {
-            name: 'Bappu',
-            age: '22'
-        }
-    )
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+// post data to MongoDB
+app.post('/create-post', async (req, res) => {
+    const result = await postCollection.insertOne(req.body);
+
+    res.json({
+        message: 'Post created successfully !',
+        data: result,
+    })
 })
 
 app.listen(port, () => {
